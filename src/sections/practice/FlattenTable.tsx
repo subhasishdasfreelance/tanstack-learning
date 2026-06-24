b78f09246916247e0b3f5a70579c3cd09b71e5b2
Subhasish Das <subhasishdasfreelance@gmail.com>
2026-06-24 13:52:29 +0530

    wip

---
+++ src/sections/practice/FlattenTable.tsx
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: 1,
+    children: [
+      {
+        id: 2
+      }
+    ]
+  }
+]
+
+export default function FlattenTable() {
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="container mx-auto px-4">FlattenTable</div>
+    </div>
+  );
+}
